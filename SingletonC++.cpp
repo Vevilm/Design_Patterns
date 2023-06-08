3c0f89f237412201128fcb1d4c5c5424fe54a290
Vevilm <117774706+Vevilm@users.noreply.github.com>
2023-06-09 00:08:57 +0300

    Singleton C++
    Пример реализации паттерна Одиночка на С++

---
+++ SingletonC++.cpp
@@ -0,0 +1,36 @@
+﻿//Код написал: Aaron Sarkissian
+#include <iostream>
+using namespace std;
+
+class Logger {
+private:
+	static Logger* instance;
+	//Создаём приватный конструктор, чтобы его нельзя было вызвать из вне класса
+	Logger() {};
+public:
+	//Статический метод доступа
+	static Logger* getInstance() {
+		if (instance == NULL) {
+			instance = new Logger();
+		}
+		return instance;
+	}
+	void log(string str) {
+		cout << "Log: " << str << endl;
+	}
+};
+
+//Экземпляр, создаваемый по требованию
+Logger* Logger::instance = NULL;
+
+int main() {
+
+	Logger* L = Logger::getInstance();
+
+	L->log("Login");
+	L->log("Like");
+	L->log("Comment");
+	L->log("Logout");
+
+	return 0;
+}
